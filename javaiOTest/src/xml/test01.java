67597bb3388a15ab22e42d6b513f2b85e43962d8
windy9109 <windy9109@naver.com>
2022-03-12 14:44:50 +0900

    no message

--- javaiOTest/src/xml/test01.java
+++ javaiOTest/src/xml/test01.java
@@ -1,12 +1,51 @@
-package xml;
+import java.io.File;
+import java.nio.file.Paths;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 public class test01 {
 
 
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+		
+        try {
+            // 파일 경로
+            File stocks = new File(Paths.get("").toAbsolutePath()+"d:/d_other/Stocks.xml");
+            // 파일 읽기
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(stocks);
+            doc.getDocumentElement().normalize();
+            
+            System.out.println("파일출력");
+            
+            // 읽어들인 파일 불러오기
+            NodeList nodes = doc.getElementsByTagName("stock");
+            for (int k = 0; k < nodes.getLength(); k++) {
+                Node node = nodes.item(k);
+                
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element element = (Element) node;
+                    System.out.println("Stock Symbol: " + getValue("symbol", element));
+                    System.out.println("Stock Price: " + getValue("price", element));
+                    System.out.println("Stock Quantity: " + getValue("quantity", element));
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    private static String getValue(String tag, Element element) {
+        NodeList nodes = element.getElementsByTagName(tag).item(0).getChildNodes();
+        Node node = (Node) nodes.item(0);
+        return node.getNodeValue();
+    }
 }
