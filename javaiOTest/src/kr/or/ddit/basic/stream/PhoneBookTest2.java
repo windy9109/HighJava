6035eceba6a90071330977f763635c63731bdaf2
windy9109 <windy9109@naver.com>
2022-03-14 17:49:22 +0900

    03-14

--- javaiOTest/src/kr/or/ddit/basic/stream/PhoneBookTest2.java
+++ javaiOTest/src/kr/or/ddit/basic/stream/PhoneBookTest2.java
@@ -1,5 +1,14 @@
 package kr.or.ddit.basic.stream;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -44,8 +53,28 @@ public class PhoneBookTest2 {
 	private Scanner sc = new Scanner(System.in); 
 	//Phone class 에 있는 변수들을 HashMap에 담는다.
 	Map<String, Phone2> list = new HashMap<String, Phone2>();
+	private String fileName ="d:/d_other/phone.dat";
+	
+	// 데이터가 변경되었는지 여부를 나타내는 변수 선언
+	// 데이터가  
+	private boolean dataChange;
 	
 
+
+	//생성자
+	public PhoneBookTest2() {
+		this.sc = sc;
+		this.list = load();
+		//파일이 없거나 잘못되었을때...
+		if(list == null) {
+			list = new HashMap<String, Phone2>();
+		}
+		
+		this.fileName = fileName;
+	}
+
+
+	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -62,6 +91,7 @@ public class PhoneBookTest2 {
 				+ "3.전화번호 삭제\n"
 				+ "4.전화번호 검색\n"
 				+ "5.전화번호 전체 출력\n"
+				+ "6.전화번호 저장\n"
 				+ "0.프로그램종료");
 		int input = sc.nextInt();
 		
@@ -71,7 +101,9 @@ public class PhoneBookTest2 {
 		case 3: return delete(); //전화번호 삭제
 		case 4: return select(); //전화번호 검색
 		case 5: return view(); //전화번호 전체출력
-		case 0: System.out.println("프로그램을 종료합니다"); System.exit(input); break; //프로그램 종료
+		case 6: return save(); //전화번호 저장
+		case 0: if( dataChange == true ) { save(); }
+			System.out.println("프로그램을 종료합니다"); System.exit(input); break; //프로그램 종료
 		default: System.out.println("잘못입력했습니다. 다시입력해주세요");
 		return start();
 			
@@ -79,6 +111,81 @@ public class PhoneBookTest2 {
 		return start();
 		
 	}
+	
+	//저장된 전화번호가 정보 파일을 읽어오는 메서드
+	// 반환값: 파일에서 읽어온 데이터( Map객체 )
+	 private HashMap<String, Phone2> load(){
+		 //읽어온 데이터가 저장될 변수 선언
+		 HashMap<String, Phone2> pMap = null;
+		 
+		 File file = new File(fileName);
+		 if(!file.exists()) { //저장된 파일이 없으면...
+			 return null; 
+		 }
+		 ObjectInputStream ois = null;
+		 try {
+			//입력용 스트림객체 생성
+			 ois = new ObjectInputStream(
+					 new BufferedInputStream(
+							 new FileInputStream(file)
+					  )
+				   );
+			 //파일 내용을 읽어서 변수에 저장한다.
+			 pMap =(HashMap<String, Phone2>)ois.readObject();
+			 
+			 
+		} catch (IOException e) {
+			return null;
+		} catch (ClassNotFoundException e) {
+			return null;
+		}finally {
+			if(ois != null) {
+				try {
+					//사용했던 스트림 닫기
+					ois.close();
+				} catch (IOException e2) {
+					// TODO: handle exception
+				}
+			}
+		}
+		 
+		 
+		 return pMap;
+		 
+	 }
+	
+	
+	 
+	 
+	//전화번호 정보를 파일로 저장하는 메서드
+	private int save(){
+		ObjectOutputStream oos = null;
+		try {
+			//객체출력용 스트림객체 생성
+			oos = new ObjectOutputStream(new BufferedOutputStream(
+					new FileOutputStream(fileName)));
+			
+			//Map객체를 파일로 저장한다.
+			oos.writeObject(list);
+			System.out.println("저장이 완료되었습니다.");
+			dataChange = false;
+			
+		} catch (IOException e) {
+			System.out.println("저장실패");
+			e.printStackTrace();
+		}finally {
+			//사용했던 스트림객체 닫기
+			if(oos!=null) {
+				try {
+					oos.close();
+				} catch (IOException e2) {
+					// TODO: handle exception
+				}
+			}
+		}
+		return 0;
+	}
+	
 	
 	//등록
 	public int add() {
@@ -92,6 +199,7 @@ public class PhoneBookTest2 {
 		int tel = Integer.parseInt(sc.nextLine());
 		list.put(name1, new Phone2(name1,addr,tel)); //Phone2 class 변수를 굳이..? 만들어줘야할까?
 		System.out.println("등록성공!");
+		dataChange = true;
 		return start();
 	}
 	
@@ -117,6 +225,7 @@ public class PhoneBookTest2 {
 			//방법2
 			//list.put(rename, new Phone2(rename,addr,tel));
 			System.out.println("수정성공!");
+			dataChange = true;
 			
 		}else{
 			System.out.println("등록되지 않은 이름입니다.");
@@ -134,6 +243,9 @@ public class PhoneBookTest2 {
 	 * 
 	*/
 	
+	
+
+	
 	//삭제
 	public int delete() {
 		System.out.println("삭제할 이름을 입력해주세요");
@@ -141,6 +253,7 @@ public class PhoneBookTest2 {
 		String name2 = sc.nextLine();
 		list.remove(name2);
 		System.out.println("삭제성공!");
+		dataChange = true;
 		return start();
 	}
 	
@@ -165,12 +278,12 @@ public class PhoneBookTest2 {
 
 
 
-
-class Phone2{
+//Phone2객체 직렬화
+class Phone2 implements Serializable{
 	private String name;
 	private String addr;
 	private int tel;
-	
+
 	
 	public Phone2(String name, String addr, int tel) {
 		super();
