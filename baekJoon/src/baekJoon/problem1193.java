c2f2a6fbe0cc12547d7201f74cd377ea94314f8e
windy9109 <windy9109@naver.com>
2022-04-28 20:22:52 +0900

    22-04-28

---
+++ baekJoon/src/baekJoon/problem1193.java
@@ -0,0 +1,16 @@
+package baekJoon;
+
+public class problem1193 {
+
+	public static void main(String[] args) {
+
+		int a = 0;
+		int b = 0;
+		
+		for(int i=0; i<Integer.MAX_VALUE; i++) {
+		
+		}
+		
+	}
+
+}
